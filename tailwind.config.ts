7df2b19d80f7a35e232c0527558e6947ef330c42
bryanguillen <bryan.guillen217@gmail.com>
2025-02-16 13:16:43 -0500

    Cleanup config file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,21 +1,15 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
     fontFamily: {
-      sans: ["Inter", "sans-serif"], // Default: Inter
-      poppins: ["Poppins", "sans-serif"], // Alternative
+      sans: ['Inter', 'sans-serif'], // Default: Inter
+      poppins: ['Poppins', 'sans-serif'], // Alternative
     },
   },
   plugins: [],
